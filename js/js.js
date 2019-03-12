5017a106c3b449b63b48abd3af7167bcbd24572b
Jacob_Wendt <jacob.wendt@yahoo.com>
2019-03-12 15:18:18 -0500

    lab07

---
+++ js/js.js
@@ -0,0 +1,60 @@
+"use strict";
+
+function a() {
+  var favoriteColor;
+  favoriteColor = prompt("Is my favorite color blue?");
+  if (favoriteColor === "yes") {
+      document.getElementById("displaya").innerHTML = "Correct!";
+  }
+  else {
+      document.getElementById("displaya").innerHTML = "False!";
+    }
+  }
+
+  function b() {
+    var favoriteColor;
+    favoriteColor = prompt("Am I 23 years old");
+    if (favoriteColor === "no") {
+        document.getElementById("displayb").innerHTML = "Correct!";
+    }
+    else {
+        document.getElementById("displayb").innerHTML = "False!";
+      }
+    }
+
+  function c() {
+    var favoriteColor;
+    favoriteColor = prompt("Is my name Jacob?");
+    if (favoriteColor === "yes") {
+        document.getElementById("displayc").innerHTML = "Correct!";
+    }
+    else {
+        document.getElementById("displayc").innerHTML = "False!";
+      }
+    }
+
+  function d() {
+    var favoriteColor;
+    favoriteColor = prompt("Do I have brown eyes");
+    if (favoriteColor === "no") {
+        document.getElementById("displaya").innerHTML = "Correct!";
+    }
+    else {
+        document.getElementById("displaya").innerHTML = "False!";
+      }
+    }
+    
+  function e() {
+    var favoriteColor;
+    favoriteColor = prompt("I have a pet cat");
+    if (favoriteColor === "yes") {
+        document.getElementById("displaya").innerHTML = "Correct!";
+    }
+    else {
+        document.getElementById("displaya").innerHTML = "False!";
+      }
+    }
+      
+              
+  
+      
